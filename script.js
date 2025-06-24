bdc1eb4f72469c7baf1523214011d6439837ef05
ANOOR111 <poncesara954@gmail.com>
2025-06-24 18:44:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const admin_username=document.querySelector('.admin_username');
+    const admin_password=document.querySelector('.admin_password');
+    const admin_submit_button=document.querySelector('.admin_submit_button');
+
+    const user='alfajr';
+    const pass="14287man";
+
+    admin_submit_button.addEventListener("click", (e)=>{
+      e.preventDefault()
+      let username=admin_username.value.trim().toLowerCase()
+      let password=String(admin_password.value)
+      if(username===user && password===pass){
+        window.location.href="dashboard.html";
+      }else{
+        alert("Invalid login details ❌❌❌")
+        
+      }
+    })
